1a0b29f72ffcca3f1200e5a57b1023f3299731b9
eko.zhan <eko.z@outlook.com>
2019-04-11 20:57:19 +0800

    feat: derby db demo

---
+++ src/main/java/com/eastrobot/heap/HeapApplication.java
@@ -0,0 +1,20 @@
+/*
+ * Power by www.xiaoi.com
+ */
+package com.eastrobot.heap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="mailto:eko.z@outlook.com">eko.zhan</a>
+ * @version 1.0
+ * @date 2019/4/11 9:36
+ */
+@SpringBootApplication
+public class HeapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeapApplication.class, args);
+    }
+}
---
+++ src/test/java/com/eastrobot/heap/HeapApplicationTests.java
@@ -0,0 +1,25 @@
+/*
+ * Power by www.xiaoi.com
+ */
+package com.eastrobot.heap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author <a href="mailto:eko.z@outlook.com">eko.zhan</a>
+ * @version 1.0
+ * @date 2019/4/11 9:36
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HeapApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
