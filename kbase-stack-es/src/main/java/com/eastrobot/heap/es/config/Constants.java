b6cef932b35e58fc76e841b9f7dbcd60a867f0e6
eko.zhan <eko.z@outlook.com>
2021-04-13 20:54:54 +0800

    feat(es): jsoup & htmlunit spider

---
+++ kbase-stack-es/src/main/java/com/eastrobot/heap/es/config/Constants.java
@@ -0,0 +1,11 @@
+package com.eastrobot.heap.es.config;
+
+/**
+ * @author <a href="mailto:eko.z@outlook.com">eko.zhan</a>
+ * @version v1.0
+ * @date 2021/4/13 19:53
+ */
+public class Constants {
+
+    public static final String INDEX_NAME = "ih_apple";
+}
