e1c55d9d25723cb52a680826b0f4dbcc23293b47
eko.zhan <eko.z@outlook.com>
2021-05-06 18:48:35 +0800

    feat(mongo): mongo gridfs download file content samples

---
+++ kbase-stack-mongo/src/main/java/com/ibothub/heap/stack/mongo/config/MongoConfig.java
@@ -0,0 +1,26 @@
+package com.ibothub.heap.stack.mongo.config;
+
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.GridFSBuckets;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @author <a href="mailto:eko.z@outlook.com">eko.zhan</a>
+ * @version v1.0
+ * @date 2021/5/6 13:48
+ */
+@Configuration
+public class MongoConfig {
+
+    @Resource
+    MongoTemplate mongoTemplate;
+
+    @Bean
+    public GridFSBucket getGridFSBucket(){
+        return GridFSBuckets.create(mongoTemplate.getDb());
+    }
+}
--- kbase-stack-mongo/src/test/java/com/ibothub/heap/stack/mongo/dao/GridFsTemplateTests.java
+++ kbase-stack-mongo/src/test/java/com/ibothub/heap/stack/mongo/dao/GridFsTemplateTests.java
@@ -1,17 +1,23 @@
 package com.ibothub.heap.stack.mongo.dao;
 
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.GridFSDownloadStream;
 import com.mongodb.client.gridfs.model.GridFSFile;
+import org.apache.commons.io.Charsets;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.gridfs.GridFsCriteria;
+import org.springframework.data.mongodb.gridfs.GridFsResource;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.ResourceUtils;
 
 import javax.annotation.Resource;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author <a href="mailto:eko.z@outlook.com">eko.zhan</a>
@@ -25,24 +31,49 @@ public class GridFsTemplateTests {
     @Resource
     private GridFsTemplate gridFsTemplate;
 
+    @Resource
+    private GridFSBucket gridFSBucket;
+
     @Test
     public void testUpload() throws IOException {
-        File file = ResourceUtils.getFile("classpath:HelloWorld.txt");
+        File file = ResourceUtils.getFile("classpath:HelloWorld2.txt");
         FileInputStream in = new FileInputStream(file);
+
 
         gridFsTemplate.store(in, file.getName());
     }
 
     @Test
-    public void testQuery(){
+    public void testQuery() throws IOException {
         Query query = new Query();
-        query.addCriteria(GridFsCriteria.whereFilename().is("HelloWorld.txt"));
+        query.addCriteria(GridFsCriteria.whereFilename().is("HelloWorld2.txt"));
         GridFSFile gridFSFile = gridFsTemplate.findOne(query);
+
         if (gridFSFile!=null){
             System.out.println(gridFSFile.getFilename());
             System.out.println(gridFSFile.getChunkSize());
             System.out.println(gridFSFile.getUploadDate());
+            System.out.println("=================================");
+            System.out.println(gridFSFile.toString());
+            System.out.println("=================================");
+            System.out.println(gridFSBucket);
+
+
+            // 打开下载流
+            GridFSDownloadStream gridFSDownloadStream = gridFSBucket.openDownloadStream(gridFSFile.getObjectId());
+            // 获取文件资源
+            GridFsResource gridFsResource = new GridFsResource(gridFSFile, gridFSDownloadStream);
+            String result = IOUtils.toString(gridFsResource.getInputStream(), StandardCharsets.UTF_8);
+            System.out.println("=================================");
+            System.out.println(result);
+            System.out.println("=================================");
+            // 关闭下载流
+            gridFSDownloadStream.close();
+
+
         }
+
+
 
     }
 }
