d1edd7570c5da03084c63c245cb7c5d1742541ad
eko.zhan <eko.z@outlook.com>
2022-02-21 20:29:36 +0800

    feat(thread): volatile 有序性验证

---
+++ kbase-stack-thread/src/main/java/com/eastrobot/heap/thread/VolatileSerialTests.java
@@ -0,0 +1,62 @@
+/*
+ * powered by https://megvii.com
+ */
+package com.eastrobot.heap.thread;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * volatile 有序性验证
+ * @author <a href="mailto:zhanzhao@megvii.com">eko.zhan</a>
+ * @version v1.0
+ * @date 2022/2/10 21:13
+ */
+public class VolatileSerialTests {
+
+
+    static int x, y, a, b;
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Set<String> resultSet = new HashSet<>();
+
+        for (int i=0;i<100000000;i++){
+            x = 0;
+            y = 0;
+            a = 0;
+            b = 0;
+
+
+            Thread one = new Thread(()->{
+               a = y;
+               x = 1;
+            });
+
+            Thread two = new Thread(()->{
+               b = x;
+               y = 1;
+            });
+
+            one.start();
+            two.start();
+
+            one.join();
+            two.join();
+
+//[a=0,b=0, a=1,b=0, a=0,b=1, a=1,b=1]
+//[a=0,b=0, a=1,b=0, a=0,b=1, a=1,b=1]
+//[a=0,b=0, a=1,b=0, a=0,b=1, a=1,b=1]
+//[a=0,b=0, a=1,b=0, a=0,b=1, a=1,b=1]
+            // 为什么会存在 a=1,b=1，两个线程同时执行时，cpu 先执行x=1，y=1，然后再进行了赋值，cpu进行了指令重排序
+            // 重排序规则：as-if-serial 和 happens-before
+            resultSet.add("a=" + a + ",b=" + b);
+            System.out.println(resultSet);
+
+
+        }
+
+    }
+
+}
