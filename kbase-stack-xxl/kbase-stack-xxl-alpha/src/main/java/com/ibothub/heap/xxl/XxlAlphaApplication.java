75edcfc4e60ef974f6faeba0f15eb48cd8dbba6c
eko.zhan <eko.z@outlook.com>
2022-06-28 20:38:43 +0800

    feat(xxl): xxl job samples

---
+++ kbase-stack-xxl/kbase-stack-xxl-alpha/src/main/java/com/ibothub/heap/xxl/XxlAlphaApplication.java
@@ -0,0 +1,17 @@
+package com.ibothub.heap.xxl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="mailto:eko.z@outlook.com">eko.zhan</a>
+ * @version v1.0
+ * @date 2022/6/16 16:23
+ */
+@SpringBootApplication
+public class XxlAlphaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlAlphaApplication.class, args);
+    }
+}
---
+++ kbase-stack-xxl/kbase-stack-xxl-alpha/src/main/java/com/ibothub/heap/xxl/schedule/JobHandler.java
@@ -0,0 +1,33 @@
+package com.ibothub.heap.xxl.schedule;
+
+import com.xxl.job.core.context.XxlJobHelper;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author <a href="mailto:eko.z@outlook.com">eko.zhan</a>
+ * @version v1.0
+ * @date 2022/6/16 16:45
+ */
+@Component
+@Slf4j
+public class JobHandler {
+
+    /**
+     * 1、简单任务示例（Bean模式）
+     */
+    @XxlJob("demoJobHandler")
+    public void demoJobHandler() throws Exception {
+        XxlJobHelper.log("XXL-JOB, Hello World.");
+
+        for (int i = 0; i < 5; i++) {
+            XxlJobHelper.log("beat at:" + i);
+            TimeUnit.SECONDS.sleep(2);
+        }
+        // default success
+    }
+
+}
