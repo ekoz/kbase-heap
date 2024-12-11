1c91f8d125daf1b706700138cf1d6eec94216969
eko.zhan <eko.z@outlook.com>
2024-12-11 15:17:11 +0800

    fix: modify redis module name

--- kbase-stack-shiro/kbase-stack-shiro-boot/src/main/java/com/ibothub/heap/shiro/config/SwaggerConfig.java
+++ kbase-stack-shiro/kbase-stack-shiro-boot/src/main/java/com/ibothub/heap/shiro/config/SwaggerConfig.java
@@ -15,9 +15,8 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 import java.util.Collections;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 /**
  * @author <a href="mailto:eko.z@outlook.com">eko.zhan</a>
@@ -25,7 +24,7 @@ import java.util.Collections;
  * @date 2020/10/15 20:25
  */
 @Configuration
-@EnableSwagger2
+@EnableSwagger2WebMvc
 public class SwaggerConfig {
 
     @Bean
